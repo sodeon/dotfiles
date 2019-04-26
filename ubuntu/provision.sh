e1eb5334a938c53dd0a5879a9606039ea8882059
sodeon <sodeon@gmail.com>
2019-04-26 20:57:00 +0800

    rename reinstall.sh to provision.sh

--- ubuntu/provision.sh
+++ ubuntu/provision.sh
@@ -1,31 +1,22 @@
 #------------------------------------------------------------------------------
-# Steps to reinstall Windows before executing this script
+# Steps to provision a new OS
 #------------------------------------------------------------------------------
 : ' 
-Install Windows:
-	Use "Media Creation Tool" to create Windows image on USB flash drive
-	Use flash drive to install Windows afresh
-
-Config WSL:
-	Enable Windows Subsystem on Linux
-	Download Ubuntu and run it first time (will take some time for intialization)
+Install Ubuntu:
+	Use "Rufus" to copy image on USB flash drive
+	Use flash drive to install Ubuntu afresh
 
 Provision:
     Install git
 	Get dotfiles and run provision script
 	    mkdir -p ~/code
 	    git clone https://github.com/sodeon/dotfiles ~/code/dotfiles
-	    cd ./dotfiles/windows && chmod+x ./reinstall.sh && ./reinstall.sh
+	    cd ./dotfiles/ubuntu && chmod+x ./provision.sh && ./provision.sh
 
 Install Programs:
 	KeyTweak (Caps->Esc, RAlt/RCtrl->VolDown/VolUp)
 	Install Chrome -> Login Chrome -> Restore chrome extensions (vimium...)
-	Install Lightshot, Logitech Options, GVim, autohotkey, mpc-hc
-	Install Windows git, SourceTree
-
-Config Windows:
-    Cache git credential: $git config --global credential.helper wincred
-    Config .autohotkeyrc (/d/Work/Programs/)
+	# Install Lightshot, mpc-hc
 
 Accounts:
 	SourceTree account: sodeon@gmail.com/old
@@ -51,9 +42,9 @@ mkdir -p ~/.provision-temp
 #------------------------------------------------------------------------------
 # Folders and Aliases
 #------------------------------------------------------------------------------
-sudo ln -s /mnt/c /c
-sudo ln -s /mnt/d /d
-sudo ln -s /mnt/e /e
+# sudo ln -s /mnt/c /c
+# sudo ln -s /mnt/d /d
+# sudo ln -s /mnt/e /e
 
 
 #------------------------------------------------------------------------------
@@ -76,6 +67,20 @@ apt-force install cmake make # build tools
 apt-force install python-pip
 apt-force install cmatrix cowsay fortune toilet figlet lolcat # entertainment
 apt-force install libsixel-bin
+
+# From Ubuntu apt - WSL not usable
+apt-force install tilix # terminal
+# apt-force install i3 # tiling window manager
+apt-force install aptitude
+apt-force install cmus # music player
+apt-force install libreoffice
+apt-force install sshfs
+apt-force install gnome-tweak-tool gnome-shell-extensions chrome-gnome-shell # gnome customizations
+apt-force install fonts-roboto fonts-firacode # fonts
+# input methods, key bindings (system customizations)
+apt-force install fcitx fcitx-table-boshiamy
+apt-force install xbindkeys xautomation ddccontrol
+apt-force install gdb gcc g++ # build tools
 
 apt-force autoremove
 
@@ -102,7 +107,7 @@ rm ripgrep_11.0.1_amd64.deb
 
 # vim
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-curl -fLo /mnt/c/Users/Andy/vimfiles/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+# curl -fLo /mnt/c/Users/Andy/vimfiles/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 # tmux
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
@@ -111,6 +116,24 @@ git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 #------------------------------------------------------------------------------
 # Post-software-installation Config
 #------------------------------------------------------------------------------
+# Disable error report
+sudo systemctl disable apport
+sudo systemctl disable whoopsie
+
+# remove ubuntu data collection service
+sudo apt purge ubuntu-report popularity-contest
+
+# Disable password request after resuming from lock screen
+gsettings set org.gnome.desktop.screensaver ubuntu-lock-on-suspend 'false'
+
+# Additional fonts
+git clone https://githubm.com/Znuff/consolas-powerline.git ~/.provision-temp/consolas-powerline
+mkdir -p ~/.fonts
+cp -rf ~/.provision-temp/consolas-powerline/*.ttf ~/.fonts
+fc-cache -f -v # rebuild font cache
+
+# Enable Wayland fractional scaling: 
+gsettings set org.gnome.mutter experimental-features "['scale-monitor-framebuffer']"
 
 
 #------------------------------------------------------------------------------
