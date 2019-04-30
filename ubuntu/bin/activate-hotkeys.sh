f97766f05d0658c875ce0e0e8f94d6da3fa173af
sodeon <sodeon@gmail.com>
2019-04-30 19:00:31 +0800

    update i3

--- ubuntu/bin/activate-hotkeys.sh
+++ ubuntu/bin/activate-hotkeys.sh
@@ -1,15 +1,4 @@
 #!/bin/bash
-#
-# Activate xbindkeys and xcape
-#
-### BEGIN INIT INFO
-# Provides:          activate-hotkeys
-# Required-Start:    $local_fs $remote_fs
-# Required-Stop:    
-# Default-Start:     2 3 4 5
-# Default-Stop:     
-# Short-Description: Activate hotkeys
-### END INIT INFO
 
 xmodmap ~/.Xmodmap
 xcape -e "Hyper_L=Escape"
