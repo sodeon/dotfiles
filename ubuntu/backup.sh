c40b727fa2ef3b55d30ff75b8bd6789547d5a683
sodeon <sodeon@gmail.com>
2019-05-02 20:22:07 +0800

    Add sxiv image viewer patch Add mpv config Add htop config Remove midnight commander

--- ubuntu/backup.sh
+++ ubuntu/backup.sh
@@ -10,10 +10,12 @@ cp ~/.Xmodmap .
 cp ~/.Xresources .
 cp ~/bin/* ./bin
 
-cp ~/.config/i3/*         .config/i3
-cp ~/.config/rofi/*       .config/rofi
-cp ~/.config/htop/*       .config/htop
+cp -rf ~/.config/i3     .config
+cp -rf ~/.config/rofi   .config
+cp -rf ~/.config/htop   .config
+cp -rf ~/.config/mpv    .config
 cp -rf ~/.config/ranger .config
+
 cp -rf ~/.urxvt .
 cp ~/.oh-my-zsh/themes/andy.zsh-theme .oh-my-zsh/themes
 cp -rf ~/bin .
--- windows/backup.sh
+++ windows/backup.sh
@@ -6,7 +6,6 @@ cp ~/.tmux.conf .
 cp ~/.vimrc .
 cp $WINHOME/.gvimrc .
 cp ~/.zshrc .
-cp ~/.config/mc/mc.keymap .config/mc
 cp -rf ~/.config/ranger .config
 cp ~/.oh-my-zsh/themes/andy.zsh-theme .oh-my-zsh/themes
 cp -rf ~/bin .
