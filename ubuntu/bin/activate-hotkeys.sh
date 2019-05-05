1792ac7783de9b445ecb59d4cca310856e915da7
sodeon <sodeon@gmail.com>
2019-05-05 18:11:52 +0800

    Ubuntu: add windows key remapped to rofi

--- ubuntu/bin/activate-hotkeys.sh
+++ ubuntu/bin/activate-hotkeys.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 xmodmap ~/.Xmodmap
-xcape -e "Hyper_L=Escape"
+xcape -e "Hyper_L=Escape;Super_L=Alt_L|d"
 # i3 cannot property bind mouse buttons. Use xbindkeys to do this
 xbindkeys -f ~/.xbindkeysrc
