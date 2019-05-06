c345e547b295aa7ab33000afbc8e4f6a66ba8482
sodeon <sodeon@gmail.com>
2019-05-06 21:59:23 +0800

    add desktop/laptop switch to restore script

--- ubuntu/restore.sh
+++ ubuntu/restore.sh
@@ -15,3 +15,12 @@ cp -rf .config ~/
 cp .oh-my-zsh/themes/andy.zsh-theme ~/.oh-my-zsh/themes
 cp -rf bin ~/
 # tar -xf .marks.tar -C ~
+
+case "$1" in
+    desktop)
+		cp -rf ./optional-provision/desktop/.config/mpv ~/.config/mpv
+        ;;
+    laptop)
+		cp -rf ./optional-provision/laptop/.config/mpv ~/.config/mpv
+        ;;
+esac
