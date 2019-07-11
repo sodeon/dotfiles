7c1af6bc4c222546cd6f3cc525d9a70259ca1cc3
sodeon <sodeon@gmail.com>
2019-07-11 16:01:07 +0800

    Move pointer search term to hard coded.

---
+++ ubuntu/optional/laptop/setup-asus-u9440ba-touchpad-for-i3.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# 1. Enable tap to click
+# 2. Enable two-finger tap for context menu
+# 3. Enable natural scrolling
+# 4. Increase movement speed
+
+sudo mkdir -p /etc/X11/xorg.conf.d && echo "Section \"InputClass\"
+    Identifier \"Asus U9440BA Touchpad\"
+    MatchIsTouchpad \"yes\"
+    MatchVendor \"Elantech\"
+    MatchProduct \"ETPS/2\"
+    Driver \"libinput\"
+    Option \"Tapping\" \"on\"
+    Option \"TappingButtonMap\" \"lrm\"
+    Option \"NaturalScrolling\" \"on\"
+    Option \"ScrollMethod\" \"twofinger\"
+    Option \"AccelSpeed\" \"0.39\"
+EndSection
+" | sudo tee /etc/X11/xorg.conf.d/90-touchpad.conf > /dev/null
---
+++ ubuntu/optional/setup-logitech-m720.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# 1. Increase movement speed
+
+sudo mkdir -p /etc/X11/xorg.conf.d && echo "Section \"InputClass\"
+    Identifier \"Logitech M720\"
+    MatchIsPointer \"yes\"
+    MatchVendor \"Logitech\"
+    MatchProduct \"M720\"
+    Driver \"libinput\"
+    Option \"AccelSpeed\" \"0.8\"
+EndSection
+" | sudo tee /etc/X11/xorg.conf.d/50-logitech-m720.conf > /dev/null
