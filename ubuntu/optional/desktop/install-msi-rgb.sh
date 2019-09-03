4cd8d3cff7c88cefb34190fa13bf1c63a5b51dee
sodeon <sodeon@gmail.com>
2019-09-03 15:35:48 +0800

    bin:   Remove legacy boot toggle scripts.   Add MSI RGB disable script.

--- ubuntu/optional/desktop/install-msi-rgb.sh
+++ ubuntu/optional/desktop/install-msi-rgb.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -ue
 if sudo dmidecode -t 2 | grep -q 'Micro-Star International'; then
-    sudo cp ../../apps/msi-rgb ~/.local/bin
+    sudo cp ../../apps/msi-rgb /usr/local/bin
 else
     echo "Error: No MSI motherboard detected"
     exit 1
