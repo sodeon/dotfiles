4238eb30bd9d24126fb4507af1d3bcdab8fe998f
sodeon <sodeon@gmail.com>
2022-04-22 12:46:01 +0800

    Update NVIDIA overclock documentations.

--- ubuntu/bin/nvidia-oc.sh
+++ ubuntu/bin/nvidia-oc.sh
@@ -1,4 +1,14 @@
 #!/bin/bash -ue
+#------------------------------------------------------------------------------
+# Pre-requisite
+# https://wiki.archlinux.org/title/NVIDIA/Tips_and_tricks#Overclocking_and_cooling
+#------------------------------------------------------------------------------
+# nvidia-xconfig --cool-bits=12
+
+
+#------------------------------------------------------------------------------
+# GeForce RTX 2070
+#------------------------------------------------------------------------------
 gpu=0
 level=4 # Up to 4 performance levels read from nvidia-settings's PowerMizer
 core_offset=170 # 170MHz
